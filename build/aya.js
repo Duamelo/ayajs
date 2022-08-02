9c516be3ab8504df869180d172bd495d9be69e99
unknown <david.dosseh@imsp-uac.org>
2022-08-02 15:29:09 +0200

    bug fix into component image

--- build/aya.js
+++ build/aya.js
@@ -3110,6 +3110,8 @@
 
 	        this.text = text;
 
+	        this.type = 'text';
+
 	        this.svg = svg;
 	        this.c_svg = "";
 
@@ -3859,16 +3861,31 @@
 	}
 
 	class Image{
-	    constructor(x = 0, y = 0, width = 50, height = 50, path, svg){
+	    constructor(x = 0, y = 0, width = 50, height = 50, path, svg, event, config){
 	        this.width = width;
 	        this.height = height;
 	        this.x = x;
 	        this.y = y;
 	        this.path = path;
 	        this.c_svg = "";
+	        this.events = {};
+	        this.nativeEvent = event;
+	        this.config = config;
+	        this.type = 'image';
 	        this.svg = svg;
-	        this.draw();
 	    }
+
+	    addEvent(event, callback){
+	        this.c_svg.addEventListener(event, callback);
+	        this.events[event] = callback;
+	    }
+	    
+	    deleteEvent(event){
+	        var callback = this.events[event];
+	        this.c_svg.removeEventListener(event, callback);
+	        delete this.events[event];
+	    }
+
 
 	    draw(){
 	        this.c_svg = document.createElementNS('http://www.w3.org/2000/svg','image');
@@ -3877,7 +3894,20 @@
 	        this.c_svg.setAttributeNS('http://www.w3.org/1999/xlink','href', this.path);
 	        this.c_svg.setAttributeNS(null,'x',this.x);
 	        this.c_svg.setAttributeNS(null,'y',this.y);
+
+	        this.addEvent("mousedown", this.nativeEvent.mouseDownCb);
+
 	        this.svg.append(this.c_svg);
+	    }
+
+	    shift(dx, dy){
+	        this.x += dx;
+	        this.y +=dy;
+	    }
+
+	    redraw(){
+	        this.c_svg.setAttributeNS(null,'x',this.x);
+	        this.c_svg.setAttributeNS(null,'y',this.y);
 	    }
 
 	    removeFromDOM(){
@@ -4064,7 +4094,7 @@
 	    }
 
 	    Image(x,y, width, height, path = ""){
-	        return new Image(x, y, width, height, path, this.svg);
+	        return new Image(x, y, width, height, path, this.svg, this.events, this.config);
 	    }
 	}
 
